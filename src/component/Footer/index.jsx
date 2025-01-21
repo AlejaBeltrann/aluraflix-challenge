b1ed8b9375dd376f980341048b8d7c513fb15406
Alejandra Beltrán <beltranascencio@gmail.com>
2025-01-21 02:35:00 -0500

    new spaces un socialmedia

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -12,22 +12,34 @@ const Card = styled.div`
     align-items: center;
     border: 1px solid rgba(0,0,0,0.9);
     border-top: 0.125rem solid #2A7AE4;
+    color: rgba(253, 253, 253, 0.9);
     `
+const SocialLinks = styled.div`
+    display: flex;
+    gap: 1rem;
+    margin-left: 1rem;
+`
+
+const FooterText = styled.h2`
+    margin: 0 2rem;
+`
+
 const Footer = () => {
     return <Card>
                 <LogoMain />
-                <h2 class="footer-texto">Desarrollado por Tatiana Beltrán - 2025 </h2>
-
-                <a href="https://github.com/alejabeltrann" target="_blank">
-                    <img src="./github.svg" alt="imagem github" />
-                </a>
-                <a href="https://www.linkedin.com/in/beltranascencio/" target="_blank">
-                    <img src="./linkedin.svg" alt="imagem linkedin" />
-                </a>
-                <a href="https://www.instagram.com/alejabeltrann/" target="_blank">
-                    <img src="./instagram.svg" alt="imagem instagram" />
-                </a>
+                <FooterText>Desarrollado por Tatiana Beltrán © 2025</FooterText>
+                <SocialLinks>
+                    <a href="https://github.com/alejabeltrann" target="_blank">
+                        <img src="./github.svg" alt="imagem github" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/beltranascencio/" target="_blank">
+                        <img src="./linkedin.svg" alt="imagem linkedin" />
+                    </a>
+                    <a href="https://www.instagram.com/alejabeltrann/" target="_blank">
+                        <img src="./instagram.svg" alt="imagem instagram" />
+                    </a>
+                </SocialLinks>
             </Card>
-  };
+};
 
 export default Footer
